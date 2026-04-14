ea68b9ee66f1fae60deeaced7a9becc0133268e9
Owen Fedchenko <owenfedchenko@gmail.com>
2026-04-14 19:32:22 -0400

    Added .gitignore. fixed file structure. Added globals.css, started a base styling waiting for frontend code for the rest of the form

--- Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ padding: '10px', background: '#333', color: 'white', textAlign: 'center' }}>
-      <p>© 2026 Visual Form Builder App</p>
-    </footer>
-  );
-}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// app/builder/page.tsx
+import React from 'react';
+import Navbar from './components/Navbar';
+import FormBuilder from './components/FormBuilder';
+
+export default function BuilderPage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Form Builder</h1>
+      <FormBuilder />
+    </div>
+  );
+}
